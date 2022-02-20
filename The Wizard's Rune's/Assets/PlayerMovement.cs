605649b8a05b985cde5289212319887a58155a38
Cesar Ramirez <98363353+Cesarex03@users.noreply.github.com>
2022-02-20 17:54:21 -0300

    Updated

--- The Wizard's Rune's/Assets/PlayerMovement.cs
+++ The Wizard's Rune's/Assets/PlayerMovement.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speedofMove = 0f;
+    [SerializeField] float speedofRunFast = 0f;
     private Camera mainCamera;
     private Vector3 pointToLookAt;
     private float rayLenght;
+    [SerializeField] private Animator playerAnimator;
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = FindObjectOfType<Camera>();
+
     }
 
     // Update is called once per frame
@@ -27,21 +30,60 @@ public class PlayerMovement : MonoBehaviour
         {
 
             Move(Vector3.forward);
+            playerAnimator.SetBool("isRun", true);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Move(Vector3.forward * speedofRunFast);
+
+                playerAnimator.SetBool("isRunFast", true);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+
+                playerAnimator.SetBool("isRunFast", false);
+            }
         }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+
+            playerAnimator.SetBool("isRun", false);
+
+
+        }
+
         if (Input.GetKey(KeyCode.S))
         {
 
             Move(Vector3.back);
+            playerAnimator.SetBool("isRunBack", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+
+            playerAnimator.SetBool("isRunBack", false);
         }
         if (Input.GetKey(KeyCode.A))
         {
 
             Move(Vector3.left);
+            playerAnimator.SetBool("isRunLeft", true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+
+            playerAnimator.SetBool("isRunLeft", false);
         }
         if (Input.GetKey(KeyCode.D))
         {
 
             Move(Vector3.right);
+            playerAnimator.SetBool("isRunRight", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+
+            playerAnimator.SetBool("isRunRight", false);
         }
     }
     void Move(Vector3 direction)
@@ -57,9 +99,9 @@ public class PlayerMovement : MonoBehaviour
 
         if (groundPlane.Raycast(cameraRay, out rayLenght))
         {
-             pointToLookAt = cameraRay.GetPoint(rayLenght);
-             Debug.DrawLine(cameraRay.origin, pointToLookAt, Color.cyan);
-             transform.LookAt(new Vector3(pointToLookAt.x, transform.position.y,pointToLookAt.z));
+            pointToLookAt = cameraRay.GetPoint(rayLenght);
+            Debug.DrawLine(cameraRay.origin, pointToLookAt, Color.cyan);
+            transform.LookAt(new Vector3(pointToLookAt.x, transform.position.y, pointToLookAt.z));
 
         }
 
